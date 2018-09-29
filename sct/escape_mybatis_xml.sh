caebcb5bcb4a3b7203b1d614ff6412b155aa02b3
Kyoungsu Lee <yikster@gmail.com>
2018-09-29 09:57:52 +0900

    Add remove isNotNull, isNotEmpty, TODO use keywards

--- sct/escape_mybatis_xml.sh
+++ sct/escape_mybatis_xml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+## TODO use keywords for replace, ex> isNotEmpty, isNotNull
 for filename in `ls *.xml`; do
 	cp $filename ${filename}.bak
-	sed -e 's/#{\([a-zA-Z0-9\-\_]*\)}/:\1/g' $filename | sed -e 's/#\([a-zA-Z0-9\-\_]*\)#/:\1/g' | sed -e 's/<\!\[CDATA\[//g' | sed -e 's/\]\]>//g' | sed -e 's/<if test=.*>//g' | sed -e 's/<\/if>//g' | sed -e '/<foreach collection/,/<\/foreach>/d' > $filename
+	sed -e 's/#{\([a-zA-Z0-9\-\_]*\)}/:\1/g' $filename | sed -e 's/#\([a-zA-Z0-9\-\_]*\)#/:\1/g' | sed -e 's/<\!\[CDATA\[//g' | sed -e 's/\]\]>//g' | sed -e 's/<if test=.*>//g' | sed -e 's/<\/if>//g' | sed -e '/<foreach collection/,/<\/foreach>/d' | sed -e 's/<isNotNull property=.*>//g' | sed -e 's/<\/isNotNull>//g' | sed -e 's/<isNotEmpty property=.*>//g' | sed -e 's/<\/isNotEmpty>//g' > $filename
 done
 
