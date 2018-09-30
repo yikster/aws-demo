9ad12e9122a1eb9815868a5bd95d239dba1b9421
Kyoungsu Lee <yikster@gmail.com>
2018-09-30 09:39:21 +0900

    Remove 100~999 parameters and close query

--- sct/txt2xml.js
+++ sct/txt2xml.js
@@ -12,6 +12,9 @@ var linenumber = 1;
 console.log('<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE mapper PUBLIC "-//mybatis.org//DTD Mapper 3.0//EN" "http://mybatis.org/dtd/mybatis-3-mapper.dtd"><mapper namespace="com.aws.sct.example">');
 reader.on('line', function (line) {
 	line = line.replace(/\s\s+/g, ' ');
+        if(line.endsWith(", ")) {
+            line = line.replace(/\:[1-9][0-9][0-9] , /g, '') + ':101 )'
+        }
 	console.log('<select id=ext_sql_id_"' + linenumber++ + '" >' + line + '</select>');
 }).on('close', function(line) {
 	console.log('</mapper></xml>');
