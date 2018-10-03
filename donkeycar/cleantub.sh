dafc681c9e5ab36f9a5b482a31d09d26a3138b5d
Kyoungsu Lee <yikster@gmail.com>
2018-10-03 23:20:14 +0900

    remove only record file

--- donkeycar/cleantub.sh
+++ donkeycar/cleantub.sh
@@ -3,3 +3,11 @@ TUB_PATH="tub/"
 ls -al $TUB_PATH | wc -l
 for i in {6800..8000}; do rm $TUB_PATH/${i}_cam-image_array_.jpg; rm $TUB_PATH/record_${i}.json; done
 ls -al $TUB_PATH | wc -l
+
+
+for i in {1..50000}; do
+        if [ -e $TUB_PATH/record_${i}.json ] && [ ! -e $TUB_PATH/${i}_cam-image_array_.jpg ]; then
+                echo "$TUB_PATH/${i}_cam-image_array_.jpg not exist. delete record_${i}.json file"
+                rm $TUB_PATH/record_${i}.json
+        fi
+done
