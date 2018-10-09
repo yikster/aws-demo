89932f4d387b0cad88f477bc151bf7189f130389
Kyoungsu Lee <yikster@gmail.com>
2018-10-10 07:52:50 +0900

    Add escape result

--- sct/escape_mybatis_xml.sh
+++ sct/escape_mybatis_xml.sh
@@ -3,11 +3,11 @@
 
 for filename in `find . -maxdepth 6 -type f -name "*.xml"`; do
         echo "${filename}"
-	grep " </dynamic>" ${filename}
-	#diff $filename ${filename}.bak
+	echo "DIFF $filename ${filename}.bak"
 	cp $filename ${filename}.bak
-	sed -e 's/#{\([a-zA-Z0-9\-\_]*\)}/:\1/g' $filename | sed -e 's/#\([a-zA-Z0-9\-\_]*\)#/:\1/g' | sed -e 's/<if test=.*>//g' | sed -e 's/<\/if>//g' | sed -e '/<foreach collection/,/<\/foreach>/d' | sed -e 's/<isNotNull .*>//g' | sed -e 's/<\/isNotNull>//g' | sed -e 's/<isNotEmpty.* prepend="\([ a-zA-Z]*\)".*>/ \1 /g' | sed -e 's/<\/isNotEmpty>//g' | sed -e 's/<isEqual.* prepend="\([ a-zA-Z]*\)".*>/ \1 /g' | sed -e 's/<\/isEqual>//g' | sed -e 's/<isNotEqual.* prepend="\([ a-zA-Z]*\)".*>/ \1 /g' | sed -e 's/<\/isNotEqual>//g' | sed -e 's/<isNotEmpty.*>//g' | sed -e 's/<isNotNull.*>//g' | sed -e 's/<isNotEqual.*>//g' | sed -e 's/<isEqual.*>//g' | sed -e 's/<isEmpty.*>//g' | sed -e 's/<\/isEmpty>//g' > $filename
-        xmllint --xpath '//sql/text()' ${filename} 
+	sed -e 's/#{\([a-zA-Z0-9\-\_]*\)}/:\1/g' $filename | sed -e 's/#\([a-zA-Z0-9\-\_]*\)#/:\1/g' | sed -e 's/<if test=.*>//g' | sed -e 's/<\/if>//g' | sed -e '/<foreach collection/,/<\/foreach>/d' | sed -e 's/<isNotNull .*>//g' | sed -e 's/<\/isNotNull>//g' | sed -e 's/<isNotEmpty.* prepend="\([ a-zA-Z]*\)".*>/ \1 /g' | sed -e 's/<\/isNotEmpty>//g' | sed -e 's/<dynamic.* prepend="\([ a-zA-Z]*\)".*>/ \1 /g' | sed -e 's/<\/dynamic>//g' | sed -e 's/<isEqual.* prepend="\([ a-zA-Z]*\)".*>/ \1 /g' | sed -e 's/<\/isEqual>//g' | sed -e 's/<isNotEqual.* prepend="\([ a-zA-Z]*\)".*>/ \1 /g' | sed -e 's/<\/isNotEqual>//g' | sed -e 's/<isNotEmpty.*>//g' | sed -e 's/<isNotNull.*>//g' | sed -e 's/<isNotEqual.*>//g' | sed -e 's/<isEqual.*>//g' | sed -e 's/<isEmpty.*>//g' | sed -e 's/<\/isEmpty>//g' > $filename
+	diff $filename ${filename}.bak
+        xmllint --xpath '//sql' ${filename} 
 	grep " prepend=" ${filename}
 done
 
