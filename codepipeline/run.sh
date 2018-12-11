6bf6fd6a3ab834830d3768184e300c085a529185
Kyoungsu Lee <yikster@gmail.com>
2018-12-11 17:11:12 +0900

    remove somthing

--- codepipeline/run.sh
+++ codepipeline/run.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-PRD_ACCOUNT_NO=174548683514
-ASSUME_ROLE_NAME=STSDeployToPrdFromDevAccount
-DEV_ACCOUNT_NO=125492839279
-REGION=ap-northeast-2
-PROJECT_ID=java-ec2-03
-PRODUCT_APP=chosun-deployclient-app
-PRODUCT_DEPLOYMENT_GROUP=chosun-deployclient-dg
-KMS_ARN="arn:aws:kms:ap-northeast-2:125492839279:key/6da53f71-17c4-46a3-9d5b-6a788dcd4a4e"
-APPROVAL_ARN="arn:aws:sns:ap-northeast-2:125492839279:APP1APPROVAL"
-
-#export AWS_PROFILE=default-chosun-map
-#python prd_sts_role.py $PROJECT_ID $DEV_ACCOUNT_NO
-
-#export AWS_PROFILE=default-ks4
-
-
 
 PRD_ACCOUNT_NO=123456789012
 ASSUME_ROLE_NAME=STSForDeployProduct
