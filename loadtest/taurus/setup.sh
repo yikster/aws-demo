2b48b92eeaeedb01397a5df87159429f38ef810c
Kyoungsu Lee <yikster@gmail.com>
2019-01-20 14:10:16 +0000

    modify install scripts

--- loadtest/taurus/setup.sh
+++ loadtest/taurus/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 ## This is tested on AmazonLinux
 ## Check your python version
-## sudo yum install python36-devel
-
+sudo yum -y remove java-1.7.0
+sudo yum -y install java-1.8.0
+sudo yum install -y python36-devel
+sudo yum install -y gcc
 sudo pip-3.6 install bzt
 export LC_ALL=en_US.UTF-8
 bzt quick_test.yml
