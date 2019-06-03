325cf2fbac0af279b3a807c4bbc65b1473dd72dd
EC2 Default User <ec2-user@ip-172-31-1-255.ap-northeast-2.compute.internal>
2019-06-03 12:36:26 +0000

    fix install scripts

--- loadtest/taurus/setup.sh
+++ loadtest/taurus/setup.sh
@@ -3,8 +3,8 @@
 ## Check your python version
 sudo yum -y remove java-1.7.0
 sudo yum -y install java-1.8.0
-sudo yum install -y python36-devel
+sudo yum install -y python3-devel.x86_64
 sudo yum install -y gcc
-sudo pip-3.6 install bzt
+sudo pip3 install bzt
 export LC_ALL=en_US.UTF-8
 bzt quick_test.yml
