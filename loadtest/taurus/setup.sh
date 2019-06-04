ce3af3e70c886955b78f761a6b0559b46b4e80fc
Kyoungsu Lee <yikster@gmail.com>
2019-06-04 04:19:45 +0000

    Add tcp rw

--- loadtest/taurus/setup.sh
+++ loadtest/taurus/setup.sh
@@ -7,4 +7,6 @@ sudo yum install -y python36-devel
 sudo yum install -y gcc
 sudo pip-3.6 install bzt
 export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+sudo sysctl -w net.ipv4.tcp_tw_reuse=1
 bzt quick_test.yml
